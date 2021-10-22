1a1213787dfedb96acbaacf15e250e760da6b01a
Geraldine <hello@geraldiner.com>
2021-10-21 21:27:15 -1000

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+const App = () => {
+	return (
+		<div>
+			<h1>Hullo</h1>
+		</div>
+	);
+};
+
+export default App;
