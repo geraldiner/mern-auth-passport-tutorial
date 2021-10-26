9d8ee1d2b783429da73ddd1dfde1b533cbd07693
Geraldine <hello@geraldiner.com>
2021-10-25 16:00:23 -1000

    finish sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -9,31 +9,34 @@ const Signup = () => {
 					<form className="mt-8 space-y-6" action="#" method="POST">
 						<div className="rounded-md shadow-sm -space-y-px">
 							<div className="input">
+								<label htmlFor="name" className="sr-only">
+									Name
+								</label>
+								<input id="name" name="name" type="text" autoComplete="name" required placeholder="Name" />
+							</div>
+							<div className="input">
 								<label htmlFor="email-address" className="sr-only">
 									Email address
 								</label>
-								<input
-									id="email-address"
-									name="email"
-									type="email"
-									autoComplete="email"
-									required
-									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-									placeholder="Email address"
-								/>
+								<input id="email-address" name="email" type="email" autoComplete="email" required placeholder="Email address" />
 							</div>
 							<div className="input">
 								<label htmlFor="password" className="sr-only">
 									Password
 								</label>
+								<input id="password" name="password" type="password" autoComplete="current-password" required placeholder="Password" />
+							</div>
+							<div className="input">
+								<label htmlFor="confirm-password" className="sr-only">
+									Confirm Password
+								</label>
 								<input
-									id="password"
-									name="password"
+									id="confirm-password"
+									name="confirm-password"
 									type="password"
 									autoComplete="current-password"
 									required
-									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-									placeholder="Password"
+									placeholder="Confirm Password"
 								/>
 							</div>
 						</div>
@@ -46,6 +49,16 @@ const Signup = () => {
 					<p className="text-white">
 						Already have an account? <a href="/login">Log in!</a>
 					</p>
+				</div>
+				<div className="login__socials">
+					<p className="text-white">Or sign up with an existing account</p>
+
+					<button type="button" className="button text-white bg-google hover:bg-white hover:text-gray-900">
+						Sign up with Google
+					</button>
+					<button type="button" className="button text-white bg-twitch hover:bg-white hover:text-gray-900">
+						Sign up with Twitch
+					</button>
 				</div>
 			</div>
 		</div>
